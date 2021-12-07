c43e404b8a1cc878f2f2123a3eb8e9b08a4d2735
Erfan Abdi <erfangplus@gmail.com>
2021-12-07 20:58:35 +0330

    hawaiip: Import and build mtk bootctrl
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>

--- device.mk
+++ device.mk
@@ -17,7 +17,11 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.1-mtkimpl.recovery
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl
 
 PRODUCT_PACKAGES += \
     bootctrl.mt6765
