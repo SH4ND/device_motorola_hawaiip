78b8d25d2d47c7cc006e180db6750fdf18333e23
Woomymy <github@woomy.ovh>
2022-06-21 17:52:55 +0200

    hawaiip: bootctrl: Update to version 1.2
    * Implement getActiveBootSlot
    
    Signed-Off-By: Woomy <github@woomy.ovh>
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>

--- device.mk
+++ device.mk
@@ -16,9 +16,9 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service \
-    android.hardware.boot@1.1-mtkimpl.recovery
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctrl
