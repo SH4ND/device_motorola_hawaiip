5dcc254e8fcfc10fc88ae2f963a9eba62c6b2b16
SH4ND <miqueiasantos20@gmail.com>
2023-04-21 13:40:30 -0300

    hawaiip: Build Thermal Libraries from Source
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>

--- device.mk
+++ device.mk
@@ -226,6 +226,11 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@2.0.vendor
+
 # VNDK
 PRODUCT_PACKAGES += \
     libcrypto-v32 \
